17b473a43d8b2d1421af8c2426c02cbf672a2032
Jameel Qureshi <jameelqureshi2013@gmail.com>
2020-07-07 20:53:46 +0500

    Mapbox sdk imported

---
+++ Assets/Scripts/DistanceCalcultor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mapbox.Unity.Location;
+using UnityEngine;
+
+public class DistanceCalcultor : MonoBehaviour {
+
+     static Location currentLocation;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public static float GetDistance()
+    {
+         
+  
+
+        // public Location CurrentLocation { get; }
+        return 0;
+    }
+}
