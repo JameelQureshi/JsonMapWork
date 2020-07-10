5b034a3573e6c21b0089c6dea9084776075d74a2
Jameel Qureshi <jameelqureshi2013@gmail.com>
2020-07-10 11:38:14 +0500

    Player Size
    Player Size fix,
    Loading Grid Color Fix

--- Assets/Mapbox/Examples/3_POIPlacement/POIPlacementScriptExample.cs
+++ Assets/Mapbox/Examples/3_POIPlacement/POIPlacementScriptExample.cs
@@ -1,37 +1,103 @@
-﻿namespace Mapbox.Examples
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Mapbox.Unity.Map;
+using Mapbox.Utils;
+using Mapbox.Unity.Utilities;
+
+//namespace Mapbox.Examples
+//{
+
+
+//	public class POIPlacementScriptExample : MonoBehaviour
+//	{
+//		public AbstractMap map;
+
+//		//prefab to spawn
+//		public GameObject prefab;
+//		//cache of spawned gameobjects
+//		private List<GameObject> _prefabInstances;
+
+//		// Use this for initialization
+//		void Start()
+//		{
+//			//add layers before initializing the map
+//			map.VectorData.SpawnPrefabByCategory(prefab, LocationPrefabCategories.ArtsAndEntertainment, 10, HandlePrefabSpawned, true, "SpawnFromScriptLayer");
+//			map.Initialize(new Vector2d(37.784179, -122.401583), 16);
+//		}
+
+//		//handle callbacks
+//		void HandlePrefabSpawned(List<GameObject> instances)
+//		{
+//			if (instances.Count > 0)
+//			{
+//				Debug.Log(instances[0].name);
+//			}
+//		}
+
+//	}
+//}
+
+
+
+
+public class POIPlacementsWSP : MonoBehaviour
 {
-	using System.Collections;
-	using System.Collections.Generic;
-	using UnityEngine;
 
-	using Mapbox.Unity.Map;
-	using Mapbox.Utils;
+    [SerializeField]
+    [Geocode]
+    string[] _TrackPartsLatitudeLongitude;
+    public string[] title;
+    public string[] body;
+    public Sprite[] PannelBG;
+    public Vector2d[] _locations;
+    [SerializeField]
+    AbstractMap _map;
+    public int _spawnScale = 1;
+    public List<GameObject> _spawnedObjects;
+    Vector2d[] _coordinates;
+    bool instatiatedmap = false;
+    public GameObject CheckpointIndicator;
 
-	public class POIPlacementScriptExample : MonoBehaviour
-	{
-		public AbstractMap map;
 
-		//prefab to spawn
-		public GameObject prefab;
-		//cache of spawned gameobjects
-		private List<GameObject> _prefabInstances;
+    private void Awake()
+    {
+        _map.OnInitialized += PLACEPOINTS;
+    }
 
-		// Use this for initialization
-		void Start()
-		{
-			//add layers before initializing the map
-			map.VectorData.SpawnPrefabByCategory(prefab, LocationPrefabCategories.ArtsAndEntertainment, 10, HandlePrefabSpawned, true, "SpawnFromScriptLayer");
-			map.Initialize(new Vector2d(37.784179, -122.401583), 16);
-		}
+    private void PLACEPOINTS()
+    {
+        _locations = new Vector2d[_TrackPartsLatitudeLongitude.Length];
+        _spawnedObjects = new List<GameObject>();
 
-		//handle callbacks
-		void HandlePrefabSpawned(List<GameObject> instances)
-		{
-			if (instances.Count > 0)
-			{
-				Debug.Log(instances[0].name);
-			}
-		}
+        int count = 0;
+        foreach (string x in _TrackPartsLatitudeLongitude)
+        {
+            _locations[count] = Conversions.StringToLatLon(x);
+            var instance = Instantiate(CheckpointIndicator);
+            instance.transform.position = _map.GeoToWorldPosition(_locations[count], false);
 
-	}
+            Debug.LogWarning(_map.GeoToWorldPosition(_locations[count], true));
+
+            _spawnedObjects.Add(instance);
+            count++;
+        }
+        instatiatedmap = true;
+
+    }
+
+
+    private void LateUpdate()
+    {
+        if (instatiatedmap)
+        {
+            int count = _spawnedObjects.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var spawnedObject = _spawnedObjects[i];
+                var location = _locations[i];
+                spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+                spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+            }
+        }
+    }
 }
---
+++ Assets/Scripts/ResetToStartPosition.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Utils;
+using Mapbox.Unity.Map;
+using Mapbox.Unity.Location;
+using Mapbox.Unity.MeshGeneration.Factories;
+using Mapbox.Unity.Utilities;
+using System;
+
+public class ResetToStartPosition : MonoBehaviour
+{
+
+    [SerializeField]
+    AbstractMap _map;
+    public Vector2d StartLatLong;
+    public float Zoom;
+    public LocationProviderFactory lpf;
+
+    void Start()
+    {
+
+#if UNITY_EDITOR
+        StartLatLong = LocationProviderFactory.Instance.EditorLocationProvider.CurrentLocation.LatitudeLongitude;
+#endif
+#if !UNITY_EDITOR
+         StartLatLong=LocationProviderFactory.Instance.DeviceLocationProvider.CurrentLocation.LatitudeLongitude;
+#endif
+        _map.Initialize(StartLatLong, 15);
+        _map.SetCenterLatitudeLongitude(StartLatLong);
+        lpf = LocationProviderFactory.Instance;
+        Zoom = _map.Zoom;
+        _map.UpdateMap(StartLatLong);
+    }
+
+    private void startatGpscentre()
+    {
+        Reset();
+
+    }
+
+    public void Reset()
+    {
+        Debug.Log("Clicked");
+        Vector2d devicelatlong;
+
+
+#if UNITY_EDITOR
+        devicelatlong = LocationProviderFactory.Instance.EditorLocationProvider.CurrentLocation.LatitudeLongitude;
+#endif
+#if !UNITY_EDITOR
+         devicelatlong=LocationProviderFactory.Instance.DeviceLocationProvider.CurrentLocation.LatitudeLongitude;
+#endif
+
+
+        if (Input.location.isEnabledByUser)
+        {
+            devicelatlong = LocationProviderFactory.Instance.DefaultLocationProvider.CurrentLocation.LatitudeLongitude;
+            _map.SetCenterLatitudeLongitude(devicelatlong);
+            _map.SetZoom(Zoom);
+            _map.UpdateMap();
+
+        }
+        else if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            devicelatlong = LocationProviderFactory.Instance.DefaultLocationProvider.CurrentLocation.LatitudeLongitude;
+            _map.SetCenterLatitudeLongitude(devicelatlong);
+            _map.SetZoom(Zoom);
+            _map.UpdateMap();
+
+        }
+    }
+}
